09afc669582e0d44017af451af3f23031a62d7dc
Achmad Yogi Prakoso <achmad.yogi.prakoso@gmail.com>
2019-05-18 08:15:26 +0700

    test lagi

--- resources/views/test2.blade.php
+++ resources/views/test2.blade.php
@@ -2,6 +2,8 @@
 	<label>Apakah Anda sudah memiliki NIM?</label>
 	<input type="radio" name="nomor_NIM" value="1" id="nomor_NIM"> Sudah<br>
 	<input type="radio" name="nomor_NIM" value="0" id="nomor_NIM"> Belum<br>';
-	?>
 
 // The test is held here
+// Another test is also here
+
+?>
