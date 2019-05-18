e2f18e5a280a2e73efd3d16c700fa2a22f8205f3
Achmad Yogi Prakoso <achmad.yogi.prakoso@gmail.com>
2019-05-18 08:13:20 +0700

    Uji coba commit

--- resources/views/test2.blade.php
+++ resources/views/test2.blade.php
@@ -3,3 +3,5 @@
 	<input type="radio" name="nomor_NIM" value="1" id="nomor_NIM"> Sudah<br>
 	<input type="radio" name="nomor_NIM" value="0" id="nomor_NIM"> Belum<br>';
 	?>
+
+// The test is held here
